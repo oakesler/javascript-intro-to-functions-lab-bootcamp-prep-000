bce85446cc83ad192811059b2e60b384b0f253f8
Olivia Kesler <oakesler@protonmail.ch>
2018-11-27 19:40:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+  
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+
+}
+
+function logShout(string) {
+  var spy = string.toUpperCase()
+  console.log(spy)
+}
+
+function logWhisper(string) {
+  var spy = string.toLowerCase()
+  console.log(spy)
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  string.toLowerCase() === lowercase
+    return "I can\'t hear you!"
+  
+  var uppercase = "HELLO"
+  string.toUpperCase() === uppercase
+    return "YES INDEED!"
+  
+  var loveyou = "I love you, Grandma."
+  string = loveyou
+    return "I love you, too."
+  
+}
